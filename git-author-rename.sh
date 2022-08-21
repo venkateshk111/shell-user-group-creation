eab139bda0c4c4a7a374840ce1f910d2c9a298aa
Venkatesh <Venkatesh111@gmail.com>
2022-08-21 20:02:03 +0530

    added script file which helps to rename git user and email id

--- git-author-rename.sh
+++ git-author-rename.sh
@@ -5,3 +5,6 @@ GIT_AUTHOR_EMAIL=venkatesh111@gmail.com;
 GIT_AUTHOR_NAME="Venkatesh"
 GIT_COMMITTER_EMAIL=$GIT_AUTHOR_EMAIL;
 GIT_COMMITTER_NAME="$GIT_AUTHOR_NAME"; fi' -- --all
+
+## Source of script
+# https://acclaim.agency/blog/change-author-and-email-of-a-git-commit
